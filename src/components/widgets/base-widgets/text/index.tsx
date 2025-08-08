13f974a572fd7553326131a4aff9573426e67bd0
mason.zhu <ws_zhu@foxmail.com>
2025-08-08 10:10:25 +0800

    feat: add text

---
+++ src/components/widgets/base-widgets/text/index.tsx
@@ -0,0 +1,37 @@
+
+import { createEditorColorProp, createEditorInputNumberProp, createEditorInputProp, createEditorSelectProp } from '@/utils/visual-editor-props';
+import { fontArr } from './fontArr';
+import { useGlobalProperties } from '@/hooks/useGlobalProperties';
+import type { VisualEditorComponent } from '@/utils';
+export default {
+  key: 'text',
+  moduleName: 'baseWidgets',
+  label: '文本',
+  preview: () => <span>预览文本</span>,
+  render: ({ props, block, styles }) => {
+    const { registerRef } = useGlobalProperties();
+
+    return () => (
+      <div
+        ref={(el) => registerRef(el, block._vid)}
+        style={{
+          color: props.color,
+          fontSize: `${parseFloat(props.size)}px`,
+          fontFamily: props.font,
+          ...styles,
+        }}
+      >
+        {props.text || '默认文本'}
+      </div>
+    );
+  },
+  props: {
+    text: createEditorInputProp({ label: '显示文本' }),
+    font: createEditorSelectProp({ label: '字体设置', options: [...fontArr] }),
+    color: createEditorColorProp({ label: '字体颜色' }),
+    size: createEditorInputNumberProp({
+      label: '字体大小',
+      defaultValue: 16,
+    }),
+  },
+} as VisualEditorComponent;
